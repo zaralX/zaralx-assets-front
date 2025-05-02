8b72812192d2541f4dd3e04af9c1d8a4454f2237
zaralX <61970128+zaralX@users.noreply.github.com>
2025-05-02 09:42:29 +0800

    social-card path fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/social-card-png',
+    image: 'img/social-card.png',
     navbar: {
       title: 'zaralX Assets',
       logo: {
